d60cbb1576b7f4d3070077c96d7b52de24b69b2f
gui1535 <guilherme.araujo1535@gmail.com>
2022-04-17 21:27:50 -0300

    Classes Moeda e Mercado

---
+++ CotacaoDolar/Classes/Mercado.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace CotacaoDolar.Classes
+{
+    public class Mercado
+    {
+
+        // Construtor
+        public Mercado()
+        {
+            Moeda = new Moeda();
+        }
+
+        // Propriedades / Atributos
+
+        [JsonProperty(PropertyName = "currencies")]
+        public Moeda Moeda { get; set; }
+
+
+    }
+}
--- CotacaoDolar/Classes/Moeda.cs
+++ CotacaoDolar/Classes/Moeda.cs
@@ -1,19 +1,24 @@
 ﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CotacaoDolar.Classes
 {
     public class Moeda
     {
-        [JsonProperty(PropertyName = Cu)]
         // Propriedades / Atributos
+
+        [JsonProperty(PropertyName = "name")]
         public string Name { get; set; }
+
+
+        [JsonProperty(PropertyName = "buy")]
         public decimal Compra { get; set; }
+
+
+        [JsonProperty(PropertyName = "sell")]
         public decimal Venda { get; set; }
+
+
+        [JsonProperty(PropertyName = "variation")]
         public decimal Variacao { get; set; }
     }
 }
